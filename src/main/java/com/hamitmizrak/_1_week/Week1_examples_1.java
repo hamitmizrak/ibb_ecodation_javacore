3013fb909587df20cad05f91952d16def5bafb4f
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-08 18:39:29 +0300

    Examples

--- src/main/java/com/hamitmizrak/_1_week/Week1_examples_1.java
+++ src/main/java/com/hamitmizrak/_1_week/Week1_examples_1.java
@@ -2,6 +2,13 @@ package com.hamitmizrak._1_week;
 
 import java.util.Scanner;
 
+/*
+1. 1. Dereceden 1 Bilinmeyenli Denklemi Çözme (Scanner kullanlalım)
+      Soru:
+      Kullanıcıdan ax + b = 0 denklemindeki a ve b değerlerini alıp x'i hesaplayan programı yazınız.
+      Çözüm:
+ */
+
 public class Week1_examples_1 {
 
     public static void main(String[] args) {
