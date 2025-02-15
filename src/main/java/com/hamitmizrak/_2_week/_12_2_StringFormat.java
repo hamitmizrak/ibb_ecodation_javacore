113c1fa142bea150ea8e74fc874ea7dc5683611a
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-15 17:52:27 +0300

    Format Specifiers

--- src/main/java/com/hamitmizrak/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/hamitmizrak/_2_week/_12_2_StringFormat.java
@@ -30,19 +30,27 @@ public class _12_2_StringFormat {
     }
 
     // formatter (Best Practice)
-    public static void formatter3(){
-
+    public static void formatter3() {
+        String formatterString = String.format("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
+        System.out.println(formatterString);
     }
 
     // formatter4
     public static void formatter4() {
-        System.out.printf("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
+        String name = "Hamit";
+        int tcNumber = 11223344;
+        double price = 44.23;
+        System.out.printf("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", name, tcNumber, price);
+        System.out.println();
+        System.out.printf("Merhabalar Adınız: %10s, T.C: %d, Fiyat: %f ", name, tcNumber, price); //%10s: Sağdan 10 karakter genişliğinde
+        System.out.println();
+        System.out.printf("%-15s %-15d %.3f ", name, tcNumber, price);//%-15s: Sola doğru yasla
     }
 
     public static void main(String[] args) {
         // formatter1();
         //formatter2();
-         formatter3();
-        //formatter4();
+        //formatter3();
+        formatter4();
     }
 }
