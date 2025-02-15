d52802de3183ed85222145644463cff6a2ee35e3
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-15 17:39:40 +0300

    System.printf("")

--- src/main/java/com/hamitmizrak/_2_week/_12_2_StringFormat.java
+++ src/main/java/com/hamitmizrak/_2_week/_12_2_StringFormat.java
@@ -15,20 +15,34 @@ public class _12_2_StringFormat {
 
     // formatter1
     public static void formatter1() {
+        // Eğer new Formatter yazarsak close() ile manuel kapatmak zorundayız.
         Formatter formatter = new Formatter();
-        formatter.format(" Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
+        formatter.format("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
         System.out.println(formatter);
         formatter.close(); // Belleği serbest bırakma
     }
 
     // formatter2 (Best Practice)
     public static void formatter2() {
-        String formatterString = String.format(" Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
+        // Eğer String.format() yazarsak kapatmak zorunda değiliz GC(Garbarage Collector) otomatik çalışır
+        String formatterString = String.format("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
         System.out.println(formatterString);
     }
 
+    // formatter (Best Practice)
+    public static void formatter3(){
+
+    }
+
+    // formatter4
+    public static void formatter4() {
+        System.out.printf("Merhabalar Adınız: %s, T.C: %d, Fiyat: %f ", "Hamit", 11223344, 44.23);
+    }
+
     public static void main(String[] args) {
-        //formatter1();
-        formatter2();
+        // formatter1();
+        //formatter2();
+         formatter3();
+        //formatter4();
     }
 }
