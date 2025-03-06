e09b0db779a2f5f017f10cd01446587e487fd2b5
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-06 21:16:36 +0300

    Other

---
+++ src/main/java/com/hamitmizrak/tutorials/_7_common/HamitMizrakException.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.tutorials._7_common;
+
+public class HamitMizrakException extends Exception{
+
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hamitmizrak/tutorials/_7_common/_1_HelloWorld.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.tutorials._7_common;
+
+public class _1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Güzel İnsanlar");
+    }
+}
