748c03ab729e2bec3562c0822f7b93fe7df52f1a
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-08 17:38:14 +0300

    Dao Optional

--- src/main/java/com/hamitmizrak/dao/IDaoGenerics.java
+++ src/main/java/com/hamitmizrak/dao/IDaoGenerics.java
@@ -1,10 +1,22 @@
 package com.hamitmizrak.dao;
 
-import com.hamitmizrak.dto.StudentDto;
-
 import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
+
+/*
+Boş olabilir (Optional.empty())
+Bir değer içerebilir (Optional.of(T value))
+*/
+
+/*
+Java 8, bazı hazır fonksiyonel arayüzler de sunar:
+
+Predicate → boolean test(T t)  → Koşul kontrolleri için.
+Function<T, R> → R apply(T t)  → Bir değeri dönüştürmek için.
+Consumer → void accept(T t)    → Parametre alır, bir işlem yapar ama geriye değer döndürmez.
+Supplier → T get()             → Parametre almaz, bir değer üretir.
+ */
 
 public interface IDaoGenerics<T> {
 
