af452208b99ce378ca34f47bf0bcf3cb45a1b056
Hamit Mızrak <hamitmizrak@gmail.com>
2026-04-23 23:10:20 +0300

    data

---
+++ src/main/java/com/hamitmizrak/tutorials/common/_1_HelloWorld.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.tutorials.common;
+
+public class _1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Merhabalar Güzel İnsanlar");
+    }
+}
